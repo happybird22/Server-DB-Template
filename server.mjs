28908ef9bd2a14b1c71a97669b75e8156fec13f3
happybird22 <dungeonsnotdating@gmail.com>
2025-04-24 09:43:38 -0600

    Server bioler plate completed

---
+++ server.mjs
@@ -0,0 +1,20 @@
+// Imports
+import express from 'express';
+import dotenv from 'dotenv';
+
+// Setups
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Midleware
+app.use(express.json());
+
+// Routes
+
+// Error Handling Middleware
+
+// Listeners
+app.listen(PORT, () => {
+    console.log('Server ')
+})
