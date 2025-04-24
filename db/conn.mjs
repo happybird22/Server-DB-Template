8e72090673b60ee5387951a75b91f12f766c8777
happybird22 <dungeonsnotdating@gmail.com>
2025-04-24 14:43:43 -0600

    added global middleware

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv'; //bring in dotenv for access to env
+import { MongoClient } from 'mongodb'; //brings in mongoclient to conn to db
 
-dotenv.config();
+dotenv.config(); //setsup env variable connection
 
-const connectionString = process.env.mongoURI;
+const connectionString = process.env.mongoURI; //grabs connString from env
 
-const client = new MongoClient(connectionString);
+const client = new MongoClient(connectionString); //creates a new mongo client with conn string
 
-let conn;
+let conn; //variable to store conn in
 
 try {
     conn = await client.connect();
@@ -18,6 +18,6 @@ try {
     console.error(err)
 }
 
-let db = conn.db('sample_training');
+let db = conn.db('sample_training'); //choosing which cluster to access
 
-export default db;
+export default db; //export connected db
--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 // Imports
 import express from 'express';
 import dotenv from 'dotenv';
+import db from './db/conn.mjs';
 
 // Setups
 dotenv.config();
@@ -11,9 +12,30 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 // Routes
+app.get('/', async (req, res) => {
+    try {
+      const results = await db.collection('grades').aggregate([
+        {
+          $project: {
+            learner_id: 1,
+            class_id: 1,
+            averageScore: { $avg: '$scores.score' }
+          }
+        }
+      ]).toArray();
+      
+      res.json(results); // Send the result to the client
+    } catch (error) {
+      console.error('Aggregation error:', error);
+      res.status(500).send('Something went wrong');
+    }
+  });   
 
 // Error Handling Middleware
-
+app.use((err, _req, res, next) => {
+    res.status(500).send("Seems like we messed up somewhere...");
+  });
+  
 // Listeners
 app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`);
